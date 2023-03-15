d71d203c209bee0e5428a21a8f682c79d724908c
Ilyas Yakup Pekguc <ilyas.pekgucnl@gmail.com>
2023-03-15 22:40:36 +0300

    setted the converting system to train our model

--- main.py
+++ main.py
@@ -9,5 +9,23 @@ filepath = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapi
 
 text = open(filepath, 'rb').read().decode(encoding='udf-8').lower() #rb stands forread Binary, the lower case is so that the text will be guessing the text in lower case for better performance (it has les letter options)
 
+#NOTE: We need to change the format of the sentences to a numerical fomat so we can use the numpy library. the model canot train on sentences and let it predict the next senctences
 
+#we're selecting a pat of the text so it's easier to train on
+text = text[300000:80000] #from karakter 300000 to 800000
+
+#we're selecting all possible charachter given in the data set (for ex. if T does not include in the text than T shouldn't be included)
+characters = sorted(set(text)) #this code filters each charachter that occurs in the data set (1 time) sorted
+
+#c = charachter, i= index
+#first we want to convert the characters to numbers, train the model and the reverse the process
+char_to_index = dict((c,i) for i, c in enumerate(characters)) 
+#explenation:
+"""
+create a dictionary, which has the character as a key and the index as a value for al the i and c's in characters
+for exp:
+{'a' : 1, 'b' : 2.....}
+"""
+
+index_to_char = dict((i,c) for i, c in enumerate(characters)) #{1 : 'a', 2 : 'b'.....}
 
